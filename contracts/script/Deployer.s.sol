fd789aa4332309b7e6cf6ba7ac700389dc3ec37b
jamiqq <s30879@pjwstk.edu.pl>
2026-09-21 16:57:56 +0200

    script implemented

--- contracts/script/Deployer.s.sol
+++ contracts/script/Deployer.s.sol
@@ -8,21 +8,13 @@ import {Voting} from "../src/Voting.sol";
 contract Deployer is Script{
     function run() public{
 
-        bytes32 root = vm.envBytes32("MERKLE_ROOT");
 
         vm.startBroadcast();
 
         HonkVerifier verifier = new HonkVerifier();
 
         Voting voting = new Voting(address(verifier));
-
-        string[] memory candidates = new string[](4);
-        candidates[0] = "bob";
-        candidates[1] = "alice";
-        candidates[2] = "kevin";
-        candidates[3] = "lara";
-        voting.createElection(candidates, root, block.timestamp, block.timestamp + 365 days);
-
+        
         vm.stopBroadcast();
     }
 }
